dd0b0583049fe02ce31351d35e06c06471d69d6c
John Anthony <askinganthony@gmail.com>
2016-01-31 20:48:58 -0800

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,19 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    10.times {@recipe.ingredients.build}
   end
 
   def create
+    require 'pry'
+    binding.pry
   end
+
+  private
+
+    def recipe_params
+      params.require(:recipe).permit(:title, ingredients_attributes:[:name, :quantity])
+    end
+
+  
 end
