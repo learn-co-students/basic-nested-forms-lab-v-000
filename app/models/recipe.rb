b2289aa6044581da5f4569521383fd1c2a935d17
HarlemSquirrel <wittyman37@yahoo.com>
2016-02-19 12:11:51 -0500

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,15 @@
 class Recipe < ActiveRecord::Base
-  has_many :ingredients 
+  has_many :ingredients
+  accepts_nested_attributes_for :ingredients
+
+  def ingredients_attributes=(new_ingredients)
+    #binding.pry
+    new_ingredients.each_value do |ing|
+      #binding.pry
+      if ing[:name] != ""
+        ing = Ingredient.create(ing)
+        ingredients << ing
+      end
+    end
+  end
 end
