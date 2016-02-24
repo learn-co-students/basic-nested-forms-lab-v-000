0321c878294c543c22db2e9e892ec5a7fb44469e
lisamarie616 <lisamarie616@gmail.com>
2016-02-24 10:24:21 -0800

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,11 @@
 class Recipe < ActiveRecord::Base
-  has_many :ingredients 
+  has_many :ingredients
+
+  accepts_nested_attributes_for :ingredients, reject_if: :all_blank
+  # def ingredients_attributes=(attributes)
+
+  #   attributes.each do |i, ing_hash|
+  #     self.ingredients.build(ing_hash)
+  #   end
+  # end
 end
