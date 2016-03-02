ba6a1d04d0a461d25c035ce4bcdd4eb724aab180
empireofryan <rjohnson2001@gmail.com>
2016-03-01 20:37:15 -0500

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,8 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
-    @recipe.ingredients.build(name: 'flour')
-    @recipe.ingredients.build(name: 'sugar')
+    @recipe.ingredients.build(name: 'First ingredient')
+    @recipe.ingredients.build(name: 'Second ingredient')
   end
 
   def create
