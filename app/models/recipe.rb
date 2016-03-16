be87fe313289b2ffe4c1e06b3946dedc94287608
NStephenson <nickstephenson90@gmail.com>
2016-03-16 18:22:00 -0400

    made all tests pass

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,19 @@
+require 'pry'
 class Recipe < ActiveRecord::Base
   has_many :ingredients 
+
+  accepts_nested_attributes_for :ingredients, :reject_if => :all_blank
+
+  # def ingredients_attributes=(ingredients)
+  #   self.ingredients.each do |ing|
+  #     if !ing.name.empty? && !ing.quantity.empty?
+  #       self.ingredients.build(ing)
+  #       self.ingredients.update(ing)
+  #     end
+  #   end
+  # end
+
+  # def ingredients_attributes
+  #   self.ingredients.map { |k,v| v }
+  # end
 end
