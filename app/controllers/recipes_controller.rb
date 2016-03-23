f704b6012f9dd59ac2a68331a771936c59701323
Dakota Martinez <dakotaleemusic@gmail.com>
2016-03-23 18:49:28 +0000

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,30 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    2.times do 
+      @recipe.ingredients.build 
+    end
   end
 
   def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.valid?
+      @recipe.save 
+      redirect_to recipe_path(@recipe)
+    else
+      render :new
+    end
+  end
+  
+  private 
+  
+  def recipe_params
+    params.require(:recipe).permit(
+      :title,
+      ingredients_attributes: [
+        :name,
+        :quantity
+      ]
+    )
   end
 end
