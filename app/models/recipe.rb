87184a5248a3b00d482003c7c336b495237a004d
authorbeard <msnolan@gmail.com>
2016-03-22 20:45:24 -0400

    all tests passing

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,18 @@
 class Recipe < ActiveRecord::Base
   has_many :ingredients 
+
+  accepts_nested_attributes_for :ingredients
+
+  def ingredients_attributes=(ingredients)
+# byebug
+    ingredients.each{|i, v| 
+      if v.none?{|k, v| v.empty?} 
+        self.ingredients<<Ingredient.create(name: v["name"], quantity: v["quantity"])
+      end
+    }
+  end
+
+  def ingredients_attributes
+    self.ingredients.collect{|i| {i.name=>i.quantity}}
+  end
 end
