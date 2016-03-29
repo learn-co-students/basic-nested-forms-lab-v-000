2aecc0fde10d0c5ab2b4f6aec5100beb78b1cd5e
esimonian <eli.simonian@gmail.com>
2016-03-29 11:48:59 -0700

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,11 @@
 class Recipe < ActiveRecord::Base
   has_many :ingredients 
+  accepts_nested_attributes_for :ingredients
+before_save :remove_empty_ingredients
+
+  def remove_empty_ingredients
+    self.ingredients = self.ingredients.select do |i|
+     i.name && i.name !='' || i.quantity && i.quantity != ""
+   end
+ end
 end
