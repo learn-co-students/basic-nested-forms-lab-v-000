8a09efa1abea0bf17802ab42d4a653e3b020808a
Brad1974 <danieltbrad@gmail.com>
2016-03-31 21:56:05 -0700

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,8 +2,8 @@ class Recipe < ActiveRecord::Base
   has_many :ingredients 
   # accepts_nested_attributes_for :ingredients, :reject_if => :all_blank
 
-  def ingredients_attributes=(ing_array)
-    ing_array.each do |i, ing_hash|
+  def ingredients_attributes=(attributes)
+    attributes.each do |i, ing_hash|
       self.ingredients.build(ing_hash) if (ing_hash[:name].present? && ing_hash[:quantity].present?)
     end
   end
