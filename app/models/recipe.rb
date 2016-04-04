ddc4fe18f596b509381029662472e39ac8c7ffc0
the-widget <brennenawana108ny@gmail.com>
2016-04-04 19:38:26 -0400

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,16 @@
 class Recipe < ActiveRecord::Base
-  has_many :ingredients 
+  has_many :ingredients
+  accepts_nested_attributes_for :ingredients
+  before_save :clear
+
+  def clear
+    self.ingredients.each do |ing|
+      if ing.name.empty?
+        ing.delete
+      end
+    end
+  end
 end
+
+
+ 
