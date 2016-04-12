00f5ce87c05c3dfa6c0fbf3e0918035fbeb4a05a
lizdenhup <lizdenhup@gmail.com>
2016-04-11 21:29:21 -0500

    all tests passing

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,10 @@
 class Recipe < ActiveRecord::Base
   has_many :ingredients 
+  accepts_nested_attributes_for :ingredients 
+  before_save :erase_empty_ingredients
+
+  def erase_empty_ingredients
+    self.ingredients = self.ingredients.select { |item| item.name && item.name != '' || item.quantity && item.quantity != '' }
+  end 
+  
 end
