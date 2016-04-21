ff421c4e89b95ae248d67f8ac7585058866f9659
michaelsimon <michael.simon@gmail.com>
2016-04-20 22:37:30 -0400

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,9 +2,4 @@ class Recipe < ActiveRecord::Base
   has_many :ingredients 
   accepts_nested_attributes_for :ingredients, reject_if: proc {|attributes| attributes[:quantity||:name].blank? }
 
-
-  #   def ingredients_attributes=(ingredients)
-  #     self.ingredients.map do |ingredient| {}
-  #   end
-  # end
 end
