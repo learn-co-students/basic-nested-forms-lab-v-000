5833d5523b02b857ccfacd06b4418a69b3a81ee7
e-baker <eric@ericbaker.me>
2016-04-23 20:37:41 -0400

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,9 @@
 class Recipe < ActiveRecord::Base
   has_many :ingredients 
   accepts_nested_attributes_for :ingredients
+  before_save :only_used_ingredients
+
+  def only_used_ingredients
+    self.ingredients = self.ingredients.find_all { |ingredient| ingredient.name != '' || ingredient.quantity != '' }
+  end
 end
