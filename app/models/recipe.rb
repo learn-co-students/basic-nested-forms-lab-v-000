67f023dee42dad950952be586c4bf2f11db82bf4
Tyler Smith <oats000@hotmail.com>
2016-04-24 21:54:02 -0400

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,14 @@
 class Recipe < ActiveRecord::Base
-  has_many :ingredients 
+  has_many :ingredients
+
+
+  def ingredients_attributes=(ingredients)
+    ingredients.each do |ingredient|
+      if ingredient[1][:name] != "" && ingredient[1][:quantity] != ""
+
+        self.ingredients << Ingredient.create(name: ingredient[1][:name], quantity: ingredient[1][:quantity])
+      end
+    end
+  end
+
 end
