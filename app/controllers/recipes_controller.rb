58ef031a796fba950d131ab10f69c0d77b6ae161
Kenneth Amper <kenn.amp@gmail.com>
2016-05-09 20:35:21 -0700

    first commit

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -12,5 +12,18 @@ class RecipesController < ApplicationController
   end
 
   def create
+		@recipe = Recipe.create(recipe_params)
+
+		if @recipe.save
+			redirect_to recipe_path(@recipe)
+		else
+			render :new
+		end
   end
+
+	private
+
+	def recipe_params
+		params.require(:recipe).permit(:title)
+	end
 end
