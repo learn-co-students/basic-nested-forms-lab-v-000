129865804bce9c019987f3e74c4bc4e434b00579
Mark <mystycs@gmail.com>
2016-05-13 15:37:31 -0400

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,10 @@
 class Recipe < ActiveRecord::Base
   has_many :ingredients
   accepts_nested_attributes_for :ingredients
+
+  before_validation :remove_empty_string
+
+def remove_empty_string
+  types.reject! { |l| l.empty? }
+end
 end
