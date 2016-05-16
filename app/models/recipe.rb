60b6b2d659ff4db9613e039628ad169ca424c9be
jasonpaulso <hello@jasonpaulsouthwell.com>
2016-05-16 16:21:12 -0700

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,5 @@
 class Recipe < ActiveRecord::Base
   has_many :ingredients 
-  accepts_nested_attributes_for :ingredients, :reject_if => lambda { |c| c[:name].blank? }
-  
+  accepts_nested_attributes_for :ingredients, :reject_if => lambda { |i| i[:name].blank? }
+
 end
