a5443bfedaf91625fa4ba01735681a016372e5ec
McN33ly <rmcneely23@gmail.com>
2016-05-16 11:47:52 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Recipe.create(title: "Chicken Parmesean")
+Recipe.create(title: "Cereal")
+Ingredient.create(name: "chicken", quantity: "4 oz")
+Ingredient.create(name: "parmesean cheese", quantity: "5 oz")
+Ingredient.create(name: "pasta sauce", quantity: "2 oz")
+Ingredient.create(name: "milk", quantity: "4 oz")
+Ingredient.create(name: "cereal", quantity: "6 oz")
