cbdc64f3ea128cb3996729b50d97deaa0b533a8c
hudbuz <hzb2@cornell.edu>
2016-05-18 19:46:25 -0400

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,17 @@
 class Recipe < ActiveRecord::Base
   has_many :ingredients 
+  accepts_nested_attributes_for :ingredients
+
+  def ingredients_attributes=(ingredient)
+
+  ingredient.each do |i|
+
+    if i[1][:name] != ""
+    binding.pry
+      self.ingredients.new(name: i[1][:name], quantity: i[1][:quantity])
+    end
+    end
+  
+
+  end
 end
