6dd6b220a5cbe5cc12fe3b218ad221295032673f
Jrodr4544 <jelson.rodriguez@gmail.com>
2016-05-31 13:56:38 -0400

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
 class Recipe < ActiveRecord::Base
   has_many :ingredients 
   # accepts nested attributes and rejects any if elements are blank
-  accepts_nested_attributes_for :ingredients, :reject_if => lambda { |i| i[:name].blank? }
+  accepts_nested_attributes_for :ingredients, :reject_if => lambda { |i| i[:name].blank? || i[:quantity].blank? }
 
 end
