26482668cef9f5c26772e2d7785275c02fe13eca
cassaram09 <cassaram09@gmail.com>
2016-06-09 15:23:46 -0400

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,12 +9,11 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
-    @recipe.ingredients.build(name: "")
-    @recipe.ingredients.build(name: "")
+    @recipe.ingredients.build
+    @recipe.ingredients.build
   end
 
   def create
-    byebug
     @recipe = Recipe.new(recipe_params)
 
     if @recipe.save
