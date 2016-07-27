c559c28f2b89a16b572983a775a271522d11038f
Christian Whitesides <whitesidescr@gmail.com>
2016-07-27 15:33:22 -0600

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,8 @@
 class Recipe < ActiveRecord::Base
-  has_many :ingredients 
+  has_many :ingredients
+  accepts_nested_attributes_for :ingredients, :reject_if => :no_additional_ingredients
+
+  def no_additional_ingredients(ingredients_attributes)
+    ingredients_attributes[:name].blank? || ingredients_attributes[:quantity].blank?
+  end
 end
