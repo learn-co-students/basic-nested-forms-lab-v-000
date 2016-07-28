1425e0fae7b4e269259b71b9441a26aa6a03e1fe
kaileighrose <kaileighrose@gmail.com>
2016-07-27 23:24:32 -0400

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,22 @@
+require 'pry'
 class Recipe < ActiveRecord::Base
   has_many :ingredients 
+  accepts_nested_attributes_for :ingredients
+
+  def ingredients_attributes=(ingredients)
+    counter = 0
+    until counter > 10 do
+      ingredient = ingredients[counter.to_s]
+      if ingredient != nil 
+        newing = Ingredient.find_or_create_by(name: ingredient["name"]) 
+        if !ingredient["quantity"].empty?
+          newing.quantity = ingredient["quantity"]
+        end
+        if newing != nil && newing.name != nil && newing.quantity != nil
+          self.ingredients << newing
+        end
+      end
+      counter += 1
+    end
+  end
 end
