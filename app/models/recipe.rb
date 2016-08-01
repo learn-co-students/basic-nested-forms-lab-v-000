d3f04e13ebe939047a476bb7de0bee757b85f9b1
nothingisfunny <if.the.kids.r.united@gmail.com>
2016-08-01 16:37:07 -0400

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,12 @@
 class Recipe < ActiveRecord::Base
   has_many :ingredients 
+  accepts_nested_attributes_for :ingredients
+
+  def ingredients_attributes=(ingredients)
+  	ingredients.each do |id, hash|
+  		ingredient = Ingredient.new(hash)
+  		self.ingredients << ingredient
+  	end
+  end
+
 end
