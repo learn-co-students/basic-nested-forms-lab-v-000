d080311d4fda4e58756de855ab8a3d797cd62759
CodeByLine <yleventhal@ymail.com>
2016-08-07 15:06:55 -0400

    Done.

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,3 +1,18 @@
 class Ingredient < ActiveRecord::Base
   belongs_to :recipe
+  has_many :recipes
+
+  def ingredients
+    @ingredients = Ingredient.all
+  end
+
+  def ingredient
+    self.try(:ingredients).try(:name)
+  end
+
+  def ingredient_quantity=(name)
+     self.ingredient = Ingredient.find_or_create_by(name: name)
+     self.ingredient = ingredient
+  end
+
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,37 @@
 class Recipe < ActiveRecord::Base
-  has_many :ingredients 
+  has_many :ingredients
+  accepts_nested_attributes_for :ingredients
+
+
+  def recipe_title
+    self.try(:recipe).try(:title)
+  end
+
+  def recipe_title=(title)
+     self.recipe = Recipe.find_or_create_by(name: name)
+     self.recipe = recipe
+  end
+
+  # def genre_name=(name)
+  #   genre = Genre.find_or_create_by(name: name)
+  #   self.genre = genre
+  # end
+  #
+  # def genre_name
+  #   self.try(:genre).try(:name)
+  # end
+  #
+  # def note_contents=(notes)
+  #   notes.each do |content|
+  #     if content.strip != ''
+  #       self.notes.build(content: content)
+  #     end
+  #   end
+  #
+  # end
+  #
+  # def note_contents
+  #   self.notes.map(&:content)
+  # end
+
 end
