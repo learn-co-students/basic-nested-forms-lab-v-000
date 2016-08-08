60dcd815f9f980644d51d70c3856dc7cdc5352ad
kaileeagray <kaileeagray@gmail.com>
2016-08-08 12:40:54 -0500

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,22 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    10.times do
+      @recipe.ingredients.build
+    end
   end
 
   def create
+    @recipe = Recipe.create(recipe_params)
+    redirect_to recipe_path(@recipe)
+  end
+
+  def recipe_params
+    params[:recipe][:ingredients_attributes].reject! { |k,v| v.values == ["", ""] }
+    params.require(:recipe).permit(:title,
+      ingredients_attributes: [
+        :name,
+        :quantity
+      ])
   end
 end
