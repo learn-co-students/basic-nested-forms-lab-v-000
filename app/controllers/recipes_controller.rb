a0bb5f8d38cd2598f21b41c72b71fe61b443a7eb
Joseph Lim <joseph.d.lim@gmail.com>
2016-08-11 15:26:49 +0000

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,5 @@
 class RecipesController < ApplicationController
+
   def show
     @recipe = Recipe.find(params[:id])
   end
@@ -9,8 +10,22 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    10.times do
+      @recipe.ingredients.build
+    end
   end
 
   def create
+    @recipe = Recipe.create(recipe_params)
+    redirect_to recipe_path(@recipe)
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(
+      :title,
+      ingredients_attributes: [
+        :name,
+        :quantity
+        ] )
   end
 end
