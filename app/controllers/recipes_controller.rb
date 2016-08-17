3f07f8a84fb5a44ec0c52db682fc621d6c4189c1
jd2rogers2 <jd2rogers2@gmail.com>
2016-08-17 10:48:22 -0400

    donzo

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,23 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    10.times do |x|
+      @recipe.ingredients.build(name: "", quantity: "")
+    end
   end
 
   def create
+    @recipe = Recipe.create(recipe_params)
+    redirect_to recipe_path(@recipe)
+  end
+
+  private
+  def recipe_params
+    params[:recipe][:ingredients_attributes].each do |x, y|
+      if y[:name] == ""
+        params[:recipe][:ingredients_attributes].delete("#{x}")
+      end
+    end
+    params.require(:recipe).permit(:title, ingredients_attributes: [:quantity, :name])
   end
 end
