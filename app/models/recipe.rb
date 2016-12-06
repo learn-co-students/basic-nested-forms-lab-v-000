549cae5c0771cda09cb674c2cd04c5558e81a60b
BrunoBoehm <bruno.boehm@gmail.com>
2016-12-06 12:58:59 +0100

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,14 @@
 class Recipe < ActiveRecord::Base
   has_many :ingredients 
+
+  # we call it ingredients_attributes not to step on the AR .ingredients method that we still need
+  # fields_for provides us with a hash, not an array!!
+  def ingredients_attributes=(ingredients_attributes)
+  	# raise ingredients_attributes.inspect
+  	ingredients_attributes.values.each do |ingredient_attributes|
+  		# we add the the ingredients collection, using mass assignment, ingredients_attributes have already been sanitized by strong params
+  		self.ingredients.build(ingredient_attributes)
+  	end
+  end
+
 end
