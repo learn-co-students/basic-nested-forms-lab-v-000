56ce5d1e076716e60004429ad21452d962b7dbff
ghaash <daniel.silverman@me.com>
2017-02-13 18:43:19 -0800

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,34 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    2.times { @recipe.ingredients.build }
   end
 
   def create
+    @recipe = Recipe.create(recipe_params)
+
+    redirect_to @recipe
   end
+
+  def edit
+    @recipe = Recipe.find(recipe_params)
+  end
+
+  def update
+    @recipe = Recipe.find(recipe_params)
+    recipe.update(recipe_params)
+    redirect_to recipe
+  end
+
+  private
+    def recipe_params
+      params.require(:recipe).permit(
+          :title,
+          ingredients_attributes: [
+            :id,
+            :name,
+            :quantity
+        ]
+      )
+    end
 end
