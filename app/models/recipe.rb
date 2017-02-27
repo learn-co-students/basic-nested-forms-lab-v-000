eb257790a32855a4c9be76899e4f2d82162e9f27
Aleksandr <aleksandr.rogachev1994@gmail.com>
2017-02-27 17:00:22 -0500

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,11 @@
 class Recipe < ActiveRecord::Base
-  has_many :ingredients 
+  has_many :ingredients
+
+  #accepts_nested_attributes_for :ingredients
+
+  def ingredients_attributes=(attrs)
+    attrs.values.each do |attr|
+      self.ingredients.build(attr)
+    end
+  end
 end
