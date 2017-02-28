ad150046b3e5b95863752dda1bd6ecfa42821c50
Anna Lansfjord <lansfjord@gmail.com>
2017-02-28 23:57:59 +0000

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,24 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    10.times {@recipe.ingredients.build}
   end
 
   def create
+    @recipe = Recipe.create(recipe_params)
+    @recipe.ingredients.each do |ingredient|
+      if ingredient.name == "" && ingredient.quantity = ""
+        ingredient.destroy
+      end
+    end
+    @recipe.save
+    redirect_to recipe_path(@recipe)
   end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, ingredients_attributes: [:name, :quantity])
+  end
+
 end
