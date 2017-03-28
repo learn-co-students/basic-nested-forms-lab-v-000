190974e287d1a0c7cc1b3b0799777e63deae87db
Alex Glover <Alex@MacBook-Pro-3.local>
2017-03-28 10:22:54 -0400

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
@@ -9,8 +10,25 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    2.times {@recipe.ingredients.build}
+    # @recipe.ingredients.build
   end
 
   def create
+    # binding.pry
+    @recipe = Recipe.create(recipe_params)
+    redirect_to recipe_path(@recipe)
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(
+      :title,
+      ingredients_attributes:[
+        :quantity,
+        :name
+      ]
+    )
   end
 end
