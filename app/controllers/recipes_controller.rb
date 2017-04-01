6c43638296909bb624d37a3a2dc520a8a4ca5b24
Sean Clarke <xseanclarke@gmail.com>
2017-04-01 18:43:13 +0000

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,9 @@
 class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
+    if @recipe.ingredients.last.try(:name)
+      @recipe.ingredients.build
+    end
   end
 
   def index
@@ -9,8 +12,17 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    2.times { @recipe.ingredients.build } # Suffices for writing it two times / This allows a recipe to be creates w/ ingredients
   end
 
   def create
+    recipe = Recipe.create(recipe_params)
+    redirect_to recipe
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, ingredients_attributes: [:id, :name, :quantity])
   end
 end
