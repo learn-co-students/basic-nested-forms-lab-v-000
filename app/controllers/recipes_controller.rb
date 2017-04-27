b102a55bdd0fe43775ba3b9b8f51c5c9531ad053
Howard DeVennish <howarddevennish@gmail.com>
2017-04-27 15:54:45 +0000

    update create method, add strong params, add getters/setters

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -12,5 +12,17 @@ class RecipesController < ApplicationController
   end
 
   def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to @recipe
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:recipe_title, ingredients_attributes: [])
   end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,24 @@
 class Recipe < ActiveRecord::Base
-  has_many :ingredients 
+  has_many :ingredients
+
+  def recipe_title
+    self.title
+  end
+
+  def recipe_title=(rec_title)
+    self.title = Recipe.find_or_create_by(title: rec_title)
+  end
+
+  def recipe_ingredient_attributes
+    self.ingredients.map{&:ingredients}
+  end
+
+  def recipe_ingredient_attributes=(rec_ingredient_attributes)
+    rec_ingredient_attributes.each do |ingredient|
+      i = ingredient.strip
+      if i != nil
+        self.ingredients.build(ingredients: ingredient)
+      end
+    end
+  end
 end
