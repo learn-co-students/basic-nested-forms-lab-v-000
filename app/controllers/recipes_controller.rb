1105000635fc859a2b62829d1cae14c8faaa8cdb
nadinesk <nadine.khattak@gmail.com>
2017-06-09 13:55:30 -0400

    changes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,11 @@
 class RecipesController < ApplicationController
+
+  def new
+    @recipe = Recipe.new
+    @recipe.ingredients.build(ingredient_type: 'main')
+    @recipe.ingredients.build(ingredient_type: 'secondary')
+  end
+
   def show
     @recipe = Recipe.find(params[:id])
   end
@@ -12,5 +19,18 @@ class RecipesController < ApplicationController
   end
 
   def create
+    recipe = Recipe.create(recipe_params)
+    redirect_to recipes_path
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title,
+      ingredient_attributes: [
+        :ingredient_1,
+        :ingredient_2    
+        ]
+        )
   end
 end
