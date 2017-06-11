282bf935e272d8950971a3d3702bbc9a2d7e0fc8
nadinesk <nadine.khattak@gmail.com>
2017-06-11 04:41:28 -0400

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,8 +2,11 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
-    @recipe.ingredients.build(ingredient_type: 'main')
-    @recipe.ingredients.build(ingredient_type: 'secondary')
+
+
+    @recipe.ingredients.build(name: 'first ingredient')
+    @recipe.ingredients.build(name: 'second ingredient')
+    #raise @recipe.inspect
   end
 
   def show
@@ -12,10 +15,6 @@ class RecipesController < ApplicationController
 
   def index
     @recipes = Recipe.all
-  end
-
-  def new
-    @recipe = Recipe.new
   end
 
   def create
@@ -27,9 +26,9 @@ class RecipesController < ApplicationController
 
   def recipe_params
     params.require(:recipe).permit(:title,
-      ingredient_attributes: [
-        :ingredient_1,
-        :ingredient_2    
+      ingredients_attributes: [
+        :name,
+        :quantity    
         ]
         )
   end
