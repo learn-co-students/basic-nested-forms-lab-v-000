f1744ab362bc91f784945db1e103c1d961a498a4
Drew Pham <drewvpham@gmail.com>
2017-06-26 18:53:18 -0500

    done

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -11,22 +11,21 @@ class RecipesController < ApplicationController
     @recipe = Recipe.new
   end
 
-    def index
-      @recipes = Recipe.all
-    end
+  def index
+    @recipes = Recipe.all
+  end
 
-    def create
-      @recipe = Recipe.new recipes_params
-      if @recipe.save
-        redirect_to @recipe
-      else
-        redirect_to :new
-      end
-    end
-
-    private
-    def recipes_params
-      params.require(:recipe).permit(:title, :ingredients_attributes => [:name, :quantity])
+  def create
+    @recipe = Recipe.new recipes_params
+    if @recipe.save
+      redirect_to @recipe
+    else
+     redirect_to :new
     end
   end
-  
+
+  private
+  def recipes_params
+    params.require(:recipe).permit(:title, :ingredients_attributes => [:name, :quantity])
+  end
+end
