3e09d99eaa82e2f6e7ea72d6517b5a097fda9d63
Aniqa Elahi <aniqaelahi@yahoo.com>
2017-07-20 04:42:01 +0000

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,19 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build(name: 'flour',quantity: '1 lbs')
+    @recipe.ingredients.build(name: 'Sugar',quantity: '1 lbs')
+
   end
 
   def create
+    @recipe = Recipe.create(recipe_params)
+    redirect_to recipes_path
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title,ingredients_attributes: [:id,:name,:quantity])
   end
 end
