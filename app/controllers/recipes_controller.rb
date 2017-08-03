be8503781adfcb320239275cfe83e3cc62ebcb71
Noah Summers <summers.noah@gmail.com>
2017-08-03 19:20:12 -0400

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,22 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build
+    @recipe.ingredients.build
   end
 
   def create
+    if @recipe = Recipe.create(recipe_params)
+      redirect_to @recipe
+    else
+      render :new
+    end
   end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, ingredients_attributes: [:id, :name, :quantity])
+  end
+
 end
