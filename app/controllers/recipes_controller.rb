7072af02da0bcee1ce079747d6350f2098381c19
gb23 <gregbenj@hotmail.com>
2017-08-30 15:03:00 -0700

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -14,5 +14,28 @@ class RecipesController < ApplicationController
   end
 
   def create
+    @recipe = Recipe.new(recipe_params)
+
+    if @recipe.save
+      redirect_to recipe_path(@recipe)
+    else
+      render :new
+    end
   end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, :ingredients_attributes => [:name, :quantity])
+  end
+
+# params = {
+#     recipe: {
+#        :title => "Pizza",
+#        :ingredient_attributes => {
+#          0 => {name: "dough", quantity: "1 lb"},
+#          1 => {name: "cheese",quantity: "0.5 lb"}
+#        }
+#     }
+# } 
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,23 @@
 class Recipe < ActiveRecord::Base
   has_many :ingredients 
+  # accepts_nested_attributes_for :ingredients
+
+
+  def ingredients_attributes=(ingredients_attributes)
+      ingredients_attributes.each do |i, ingredient_attributes|
+        self.ingredients.build(ingredient_attributes)
+      end
+  end
+
+
+# params = {
+#     recipe: {
+#        :title => "Pizza",
+#        :ingredient_attributes => {
+#          0 => {name: "dough", quantity: "1 lb"},
+#          1 => {name: "cheese",quantity: "0.5 lb"}
+#        }
+#     }
+# } 
+
 end
