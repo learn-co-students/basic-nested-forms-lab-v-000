2a7e6383b38d54717ae16302fa889f901b64c39d
Jennifer Haddock <jentx123@hotmail.com>
2017-09-17 23:03:47 +0000

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,25 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build
+    @recipe.ingredients.build
   end
 
   def create
+    if @recipe = Recipe.create(recipe_params)
+      redirect_to recipe_path(@recipe)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(
+    :title,
+    ingredients_attributes: [
+      :name,
+      :quantity])
   end
 end
