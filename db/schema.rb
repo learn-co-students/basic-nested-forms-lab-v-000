e588a331ff6f41f4a02ca08c14bc57a77dc25df5
Alec Alba <alecgalba@gmail.com>
2017-09-26 17:52:10 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160114135651) do
+ActiveRecord::Schema.define(version: 20170926012240) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20160114135651) do
     t.datetime "updated_at", null: false
     t.integer  "recipe_id"
   end
+
+  add_index "ingredients", ["recipe_id"], name: "index_ingredients_on_recipe_id"
 
   create_table "recipes", force: :cascade do |t|
     t.string   "title"
