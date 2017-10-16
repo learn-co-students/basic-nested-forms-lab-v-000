a2e752c87eb8d0d56c6b0371cc702ec1b44ee62c
charlie lee <ctlee9096@gmail.com>
2017-10-16 16:53:51 +0000

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,9 +8,21 @@ class RecipesController < ApplicationController
   end
 
   def new
-    @recipe = Recipe.new
+   @recipe = Recipe.new
+   2.times do
+   @recipe.ingredients.build(recipe_id: @recipe.id)
+    end
   end
-
   def create
+    # raise params.inspect
+    @recipe = Recipe.create(recipe_params)
+    if @recipe.save#is saved correctly
+      redirect_to @recipe#take to url /person/@user_id
+    else
+      render :new
+    end
+  end
+  def recipe_params#
+    params.require(:recipe).permit(:title,:ingredients_attributes => [:name, :quantity])
   end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,10 @@
 class Recipe < ActiveRecord::Base
-  has_many :ingredients 
+  has_many :ingredients
+  def ingredients_attributes=(ingredients_attributes)
+		  #raise ingredients_attributes.inspect
+			ingredients_attributes.each do |i, ingredient|
+				#want to create an adress attributes for each person
+		 self.ingredients.build(ingredient)
+		end
+	end
 end
