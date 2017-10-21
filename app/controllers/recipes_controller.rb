32bb5514b34922375447793596dceca41f911fc0
burrahey <burra.archana@gmail.com>
2017-10-21 13:51:07 -0400

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,18 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build
+    @recipe.ingredients.build
+
   end
 
   def create
+    @recipe = Recipe.create(post_params)
+    redirect_to @recipe
+  end
+
+  private
+  def post_params
+    params.require(:recipe).permit(:title, ingredients_attributes: [:name, :quantity])
   end
 end
