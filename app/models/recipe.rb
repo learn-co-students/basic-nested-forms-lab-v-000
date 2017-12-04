0f5d9f02a6aeacc8215d9762d76b9d6cac832c3b
mandlerm <amaya.melissa@gmail.com>
2017-12-04 17:04:18 -0600

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,12 @@
 class Recipe < ActiveRecord::Base
-  has_many :ingredients 
+  has_many :ingredients
+  accepts_nested_attributes_for :ingredients
+
+  def ingredients_attributes=(ingr)
+      ingr.each do |i,ingredient|
+
+      self.ingredients << Ingredient.find_or_create_by(name: ingredient['name'], quantity: ingredient['quantity'])
+   # self.ingredients.update(ingredient)  #explain this line.  from Basic Nested Forms ReadMe
+    end
+  end
 end
