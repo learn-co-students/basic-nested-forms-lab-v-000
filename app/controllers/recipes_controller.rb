9de73c40e87fb97221887e71dc4f68f59182e517
Janusz Szubert <theinvalidnonce@gmail.com>
2017-12-13 19:41:10 +0000

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -24,8 +24,7 @@ class RecipesController < ApplicationController
   def recipe_params
     params.require(:recipe).permit(
       :title,
-      ingredients_attributes: 
-        [:name, :quantity ]
+      ingredients_attributes: [:name, :quantity ]
       )
   end
   
