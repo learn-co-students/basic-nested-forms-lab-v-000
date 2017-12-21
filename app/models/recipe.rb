95bf148d310c07ae9b3ae184043bd15e96a6f94a
Oski <github email address>
2017-12-21 06:12:27 -0500

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,12 @@
 class Recipe < ActiveRecord::Base
   has_many :ingredients 
+  accepts_nested_attributes_for :ingredients
+
+
+  #def ingredients_attributes=(ingredients)
+   # ingredients.each do |k, ingredient|
+    #  ingredient = Ingredient.create_or_find_by(name: ingredient)
+     # self.ingredients << ingredient
+    #end
+  #end
 end
