f577076904fc1506a6e1e95e91c8f23991fd6749
jonfoster9999 <redcabinny@gmail.com>
2017-12-26 16:45:49 -0500

    Update recipe.rb

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,8 +4,13 @@ class Recipe < ActiveRecord::Base
 
 
   def ingredients_attributes=(ingredients_attributes)
-  	ingredients_attributes.each do |i, ingredient_attributes|
-  		self.ingredients.build(ingredient_attributes)
-  	end
+    ingredients_names = []
+    ingredients_attributes.each do |index, ingredient_hash|
+      if !ingredients_names.include?(ingredient_hash["name"]) 
+        ingredients_names << ingredient_hash["name"]
+        ingredient = Ingredient.create(ingredient_hash)
+        self.ingredients << ingredient
+      end
+    end
   end
 end
