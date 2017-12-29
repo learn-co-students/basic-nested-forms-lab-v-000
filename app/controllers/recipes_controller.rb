334b09592a7123213244edeca267d5bfb68321f1
EC2 Default User <ec2-user@ip-172-31-35-66.us-east-2.compute.internal>
2017-12-29 04:02:04 +0000

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -15,13 +15,7 @@ class RecipesController < ApplicationController
 
   def create
     @recipe=Recipe.create(recipe_params)
-    # @recipe = Recipe.new
-    # @recipe.ingredients_attributes=(params[:recipe][ingredients_attributes])
-    # if @recipe.save
-      redirect_to recipe_path(@recipe)
-    # else 
-    #   render :new 
-    # end 
+    redirect_to recipe_path(@recipe)
   end
   
   private 
