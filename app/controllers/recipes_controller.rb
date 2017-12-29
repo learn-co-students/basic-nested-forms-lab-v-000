018f393a7c7120526dd005a16ffb28b9d0292d2e
EC2 Default User <ec2-user@ip-172-31-35-66.us-east-2.compute.internal>
2017-12-29 03:32:02 +0000

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,31 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredient.build
+    @recipe.ingredient.build
   end
 
   def create
+    @recipe=Recipe.new
+    @recipe.ingredients_attributes=(params[:recipe][ingredients_attributes])
+    if @recipe.save
+      redirect_to recipe_path
+    else 
+      render :new 
+    end 
   end
+  
+  private 
+    def recipe_params
+      params.require(:recipe).permit(
+        :title,
+        ingredients_attributes: [
+          :name,
+          :quantity
+          ]
+        )
+      
+    end 
+  
+  
 end
