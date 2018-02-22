b0a367ee76753aa78592389165a86e15c374e153
Alex <github email address>
2018-02-22 12:42:35 -0500

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,6 @@
 class RecipesController < ApplicationController
   def show
+#binding.pry
     @recipe = Recipe.find(params[:id])
   end
 
@@ -9,8 +10,20 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build #builds shell for the new views form
+    @recipe.ingredients.build #builds shell for the new views form
   end
 
   def create
+    @recipe = Recipe.create(recipe_params)
+#binding.pry
+    redirect_to recipe_path(@recipe)
+  end
+
+  private
+  #strong params so people cant mess with forms and html hacking
+  def recipe_params
+      #ingredients_attributes defined in the recipe model and the recipe new vies file
+      params.require(:recipe).permit(:title, ingredients_attributes: [:name, :quantity])
   end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,4 @@
 class Recipe < ActiveRecord::Base
-  has_many :ingredients 
+  has_many :ingredients
+  accepts_nested_attributes_for :ingredients #methods to be included for building forms that accept multiple ingredients
 end
