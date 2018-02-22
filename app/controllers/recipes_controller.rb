6b7fe72f61012d3044f420a89240b379d4e3b080
Alex <github email address>
2018-02-22 13:13:23 -0500

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,6 +2,10 @@ class RecipesController < ApplicationController
   def show
 #binding.pry
     @recipe = Recipe.find(params[:id])
+binding.pry
+    if @recipe.ingredients.last.try(:name)  #if the recipe's ingredient does not have a name
+        @recipe.ingredients.build
+    end
   end
 
   def index
@@ -12,18 +16,27 @@ class RecipesController < ApplicationController
     @recipe = Recipe.new
     @recipe.ingredients.build #builds shell for the new views form
     @recipe.ingredients.build #builds shell for the new views form
+    #or
+    # =>  2.times {@recipe.ingredients.build}
   end
 
   def create
     @recipe = Recipe.create(recipe_params)
 #binding.pry
     redirect_to recipe_path(@recipe)
+    #or
+    # =>    recipe = Recipe.create(recipe_params)
+    # =>    redirect_to recipe
   end
-
+  def update
+        @recipe = Recipe.find(params[:id])
+        recipe.update(recipe_params)
+        redirect_to recipe_path(@recipe)
+  end
   private
   #strong params so people cant mess with forms and html hacking
   def recipe_params
       #ingredients_attributes defined in the recipe model and the recipe new vies file
-      params.require(:recipe).permit(:title, ingredients_attributes: [:name, :quantity])
+      params.require(:recipe).permit(:title, ingredients_attributes: [:id, :name, :quantity])
   end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,12 @@
 class Recipe < ActiveRecord::Base
   has_many :ingredients
   accepts_nested_attributes_for :ingredients #methods to be included for building forms that accept multiple ingredients
+
+
+  #following was added because it was in solution
+  before_save :erase_empty_ingredients
+
+  def erase_empty_ingredients
+    self.ingredients = self.ingredients.select {|i| i.name && i.name != '' || i.quantity && i.quantity !=''}
+  end
 end
