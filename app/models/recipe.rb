83514d59d5e68dbe2355813136d4af3152cb8954
Thomas Viles <thomas.c.viles.jr@gmail.com>
2018-03-02 14:04:42 +0000

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,14 @@
 class Recipe < ActiveRecord::Base
-  has_many :ingredients 
+  has_many :ingredients
+  before_save :erase_empty_ingredients
+
+  def ingredients_attributes= (ingredients_attributes)
+    ingredients_attributes.each do |i, ingredient_attributes|
+      self.ingredients.build(ingredient_attributes)
+    end
+  end
+
+  def erase_empty_ingredients
+    self.ingredients = self.ingredients.select {|i| i.name != '' || i.quantity != ''}
+  end
 end
