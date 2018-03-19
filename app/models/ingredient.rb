8b061102ec451b93dd9c8f0e9f9aea0dd6388a62
github username <github email address>
2018-03-19 10:48:05 -0700

    Done.

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,6 +1,4 @@
 class Ingredient < ActiveRecord::Base
   belongs_to :recipe
 
-  validates :name, presence: true
-  validates :quantity, presence: true
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
 class Recipe < ActiveRecord::Base
   has_many :ingredients
-
-  validates :title, presence: true
   accepts_nested_attributes_for :ingredients
+
+  
 end
