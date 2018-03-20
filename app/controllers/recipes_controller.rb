ab0f1e9f79766224ebd2a8f7bfe02413e615a4a8
jacobsilver2 <jacobsilver2@mac.com>
2018-03-20 14:06:10 -0400

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -15,8 +15,12 @@ class RecipesController < ApplicationController
 
   def create
     recipe = Recipe.create(recipe_params)
-    redirect_to recipe_path
+    
+    redirect_to recipe_path(recipe)
   end
+
+  
+
 
   private
   def recipe_params
