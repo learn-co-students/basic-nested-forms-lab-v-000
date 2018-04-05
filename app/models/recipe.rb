84792679c7cf4d97e598ff9a002abbd23ace2a8e
Alli Tzadok <allisontzadok@gmail.com>
2018-04-05 18:02:21 -0500

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,10 @@
 class Recipe < ActiveRecord::Base
-  has_many :ingredients 
+  has_many :ingredients
+
+  def ingredients_attributes=(ingredients_attributes)
+    ingredients_attributes.each do |i, ingredients_attributes|
+      self.ingredients.build(ingredients_attributes)
+    end
+  end
+  
 end
