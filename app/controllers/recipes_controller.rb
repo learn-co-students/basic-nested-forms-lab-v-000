9cd66e69f6cfcdd466191da813674c1a752e313b
Katrina Brinson <kbrinso@gmail.com>
2018-04-15 21:10:46 +0000

    1 failing

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,21 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build(quantity: :quantity)
+    @recipe.ingredients.build(name: :name)
   end
 
   def create
+    recipe = Recipe.create(recipe_params)
+    redirect_to recipes_path
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, ingredients: [
+      :name,
+      :quantity
+      ])
   end
 end
