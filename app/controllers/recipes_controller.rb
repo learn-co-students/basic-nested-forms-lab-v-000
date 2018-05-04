69ec30083873d9c84695cafeaea32b7aac987d10
github username <github email address>
2018-05-04 18:12:24 -0400

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,25 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build[:name]
+    @recipe.ingredients.build[:quantity]
   end
 
   def create
+    recipe = Recipe.create(recipe_params)
+      redirect_to recipe
   end
+
+  def update
+      @recipe = Recipe.find(params[:id])
+      @recipe.update(receipe_params)
+    end
+
+ private
+
+ def recipe_params
+   params.require(:recipe).permit(:title, ingredients_attributes: [:name, :quantity])
+ end
+
+
 end
