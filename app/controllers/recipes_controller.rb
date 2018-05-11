5e4cce430d4dc137554896a31071db18a0b36a85
EC2 Default User <ec2-user@ip-172-31-32-154.us-east-2.compute.internal>
2018-05-11 17:28:40 +0000

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,18 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build(name: "First Ingredient Placeholder", quantity: 0)
+    @recipe.ingredients.build(name: "Second Ingredient Placeholder", quantity: 0)
   end
 
   def create
+    recipe = Recipe.create(rec_params)
+    redirect_to recipe
+  end
+  
+  private
+  
+  def rec_params
+    params.require(:recipe).permit(:title, ingredients_attributes: [:id, :name, :quantity])
   end
 end
