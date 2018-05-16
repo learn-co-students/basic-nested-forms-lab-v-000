163190c8725ddb4d0725a8641a18d3608b22d3e4
roitblatari <roitblatari@gmail.com>
2018-05-15 20:10:23 -0400

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,8 @@
 class Recipe < ActiveRecord::Base
   has_many :ingredients 
+  # accepts_nested_attributes_for :ingredients 
+  # def ingredients_attributes
+  #   binding.pry
+  #   self.ingredients    
+  # end
 end
