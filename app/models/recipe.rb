5553d87243d3a6bb2254e424ee8035779b591988
Joanne Lee <vernapii@gmail.com>
2018-05-16 07:50:33 -0700

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,19 @@
 class Recipe < ActiveRecord::Base
-  has_many :ingredients 
+  has_many :ingredients
+  accepts_nested_attributes_for :ingredients
+  # def ingredient_attributes=(attributes)
+  #   byebug
+  #   attributes.each do |attr|
+  #     # byebug
+  #     if !attr.empty?
+  #       ingredient = Ingredient.create(attr)
+  #       self.ingredients << ingredient
+  #     end
+  #   end
+  #   self.ingredients
+  # end
+  #
+  # def ingredient_attributes
+  #   self.try(:ingredient).try(:name, :quantity)
+  # end
 end
