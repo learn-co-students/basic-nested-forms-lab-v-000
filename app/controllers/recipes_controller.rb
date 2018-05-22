af6eaf48145b6e093ae8aa291dc2e56d2ae3a4f3
ElleeB <laura.trager@gmail.com>
2018-05-22 12:36:01 -0400

    Troubleshooting #create recipe_path

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
@@ -9,8 +11,18 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build(name: '')
   end
 
   def create
+    @recipe = Recipe.create(recipe_params)
+
+    redirect_to recipe_path
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, ingredient_attributes: [:ingredient_0_name, :ingredient_0_quantity, :ingredient_1_name, :ingredient_1_quantity ])
   end
 end
