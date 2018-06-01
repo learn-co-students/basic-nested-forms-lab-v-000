d989913b99a154f0cdfa5ca518542451147d35b1
Aly Alidina <aly_alidina@hotmail.com>
2018-06-01 22:31:31 +0000

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,18 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build(name: "Pasta Salad", quantity: "1 cup")
+    @recipe.ingredients.build(name: "Pasta Salad 2", quantity: "2 cups")
   end
 
   def create
+    @recipe = Recipe.create(recipe_parmas)
+    redirect_to recipe_path(@recipe)
+  end
+
+  private
+
+  def recipe_parmas
+    params.require(:recipe).permit(:title)
   end
 end
