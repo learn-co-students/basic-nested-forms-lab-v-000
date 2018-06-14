1e06e899abaea1f395b895af4925a3230d472d7f
Gingertonic <bethmschofield@gmail.com>
2018-06-14 11:32:11 +0930

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,25 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build
+    @recipe.ingredients.build
   end
 
   def create
+    # byebug
+    @recipe = Recipe.new(recipe_params(params))
+
+    if @recipe.valid?
+      @recipe.save
+      redirect_to recipe_path(@recipe)
+    else
+      render :new
+    end
+  end
+
+
+  private
+  def recipe_params(params)
+    params.require(:recipe).permit(:title, ingredients_attributes: [:name, :quantity])
   end
 end
