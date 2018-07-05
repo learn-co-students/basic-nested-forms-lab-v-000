b74176a8e55bcdb7e887d137bc6148cd353780c9
DGMarkel <davidgmarkel@gmail.com>
2018-07-04 23:28:58 -0400

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,12 @@
 class Recipe < ActiveRecord::Base
-  has_many :ingredients 
+  has_many :ingredients
+
+  def ingredient_name=(name)
+    self.ingredients << Ingredient.find_or_create_by(name: name)
+  end
+
+  def ingredient_name
+    self.ingredient ? self.ingredient.name : nil
+  end 
+
 end
