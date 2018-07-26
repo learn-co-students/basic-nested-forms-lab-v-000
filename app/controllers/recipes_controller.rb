663a63a59b2c54c7e19bfc69f6c1f5ec57b36b05
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-07-26 19:46:57 +0000

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -14,5 +14,18 @@ class RecipesController < ApplicationController
   end
 
   def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to recipe_path(@recipe)
+    end
   end
+  
+  
+  private 
+  
+  def recipe_params
+    params.require(:recipe).permit(:title, ingredients_attributes: [:name, :quantity])
+  end
+  
+  
 end
