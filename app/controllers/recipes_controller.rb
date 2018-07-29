e86ccb9a4887b4e61709af35ea31af18772b6bcf
taryn yaeger <tsyaeger@gmail.com>
2018-07-29 02:49:08 +0000

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -10,7 +10,23 @@ class RecipesController < ApplicationController
   def new
     @recipe = Recipe.new
   end
+  
+  def new
+    @recipe = Recipe.new
+    @recipe.ingredients.build(address_name: 'recipe_1')
+    @recipe.ingredients.build(address_quantity: 'recipe_2')
+  end
 
   def create
+    @recipe = Recipe.create(recipe_params)
+    redirect_to @recipe
   end
 end
+
+  private 
+  
+  def recipe_params 
+    params.require(:recipe).permit(:name, ingredients => [])
+
+
+
