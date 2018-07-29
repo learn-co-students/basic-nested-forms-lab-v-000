e30cf086bc3625ea02862bd2473494dbfc064692
taryn yaeger <tsyaeger@gmail.com>
2018-07-29 03:29:28 +0000

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,42 +1,47 @@
 class RecipesController < ApplicationController
-  
   def show
     @recipe = Recipe.find(params[:id])
+    if @recipe.ingredients.last.try(:name)
+      @recipe.ingredients.build
+    end
   end
 
   def index
     @recipes = Recipe.all
   end
 
-  
   def new
     @recipe = Recipe.new
-    @recipe.ingredients.build(name: 'recipe 1')
-    @recipe.ingredients.build(name: 'recipe 2')
+    @recipe.ingredients.build(name: "recipe 1")
+    @recipe.ingredients.build(name: "recipe 2")
   end
 
   def create
-    @recipe = Recipe.create(recipe_params)
-    if @recipe.save 
-      redirect_to recipe_path(@recipe)
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to @recipe
     else 
-      render :new
+      render :new 
     end
   end
 
-
-  private 
-  
-  def recipe_params 
-    params.require(:recipe).permit(
-      :name, 
-      ingredients_attributes: [
-        :name,
-        :quantity
-        ])
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(recipe_params)
+      redirect_to recipe
+    else 
+      render :edit 
+    end
   end
-        
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(
+      :title, 
+      ingredients_attributes:[
+        :name, 
+        :quantity])
+  end
+
 end
-
-
-
