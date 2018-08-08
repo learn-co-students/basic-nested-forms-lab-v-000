772d78908ae369caf3764d44dc1cb36d0690271d
ElvinGarcia <dimix181@gmail.com>
2018-08-08 17:52:57 -0400

    done

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,5 @@
 class Recipe < ActiveRecord::Base
-  has_many :ingredients 
+  has_many :ingredients, dependent: :destroy
+  accepts_nested_attributes_for :ingredients
+  validates :title, uniqueness: true
 end
