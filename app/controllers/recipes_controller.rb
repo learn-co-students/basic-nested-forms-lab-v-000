bc86f143cb464d8832db432c622a82e3b74b1803
asabogal <asabogal.git@gmail.com>
2018-09-19 10:35:57 -0400

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,25 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build
+    @recipe.ingredients.build
   end
 
   def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      flash[:notice] = "recipe successfully created"
+      redirect_to @recipe
+    else
+      flash[:notice] = "must include title"
+      render :new
+    end
   end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, ingredients_attributes: [:name, :quantity])
+  end
+
 end
