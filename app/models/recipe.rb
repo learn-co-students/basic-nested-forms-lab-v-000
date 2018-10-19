4519a607b10dfc3f0e244404ed5fedf6488658f9
Micah Shute <micah.shute@gmail.com>
2018-10-19 09:57:33 -0500

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,12 @@
 class Recipe < ActiveRecord::Base
   has_many :ingredients 
+  accepts_nested_attributes_for :ingredients
+
+  def ingredients_attributes=(ingredients_data)
+    ingredients_data.each do |index, data|
+      i = Ingredient.new(data)
+      self.ingredients << i if i.save
+    end
+  end
+
 end
