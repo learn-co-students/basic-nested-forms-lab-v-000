c509d7ddb63eadf598b5990ec7e63c58b8ffe808
Ludmilla Aires Negri <ludaires@gmail.com>
2018-11-10 16:25:17 -0500

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,26 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    # binding.pry
+    @recipe.ingredients.build(recipe_id: @recipe.id)
+    @recipe.ingredients.build(recipe_id: @recipe.id)
   end
 
   def create
+    # raise params.inspect
+    @recipe = Recipe.create(recipes_params)
+    redirect_to recipe_path(@recipe)
   end
+
+  private 
+  def recipes_params
+    params.require(:recipe).permit(
+      :title,
+      ingredients_attributes:[
+        :name,
+        :quantity
+      ]
+    )
+  end
+
 end
