ccba80e273b8f46c03916906ae4546d448ab0f4e
github username <github email address>
2018-12-17 05:27:48 -0500

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,24 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build(name: "Ingredient 1")
+    @recipe.ingredients.build(name: "Ingredient 2")
   end
 
   def create
+    @recipe = Recipe.create(recipe_params)
+
+    redirect_to recipe_path(@recipe)
   end
+
+  def recipe_params
+    params.require(:recipe).permit(
+      :title,
+        ingredients_attributes: [
+          :quantity,
+          :name,
+        ]
+      )
+  end
+
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,14 @@
+require 'pry'
 class Recipe < ActiveRecord::Base
-  has_many :ingredients 
+  has_many :ingredients
+  accepts_nested_attributes_for :ingredients
+
+
+#   def ingredients_attributes=(ingredients)
+#     binding.pry
+#     self.ingredients = Ingredient.find_or_create_by(name: ingredient.name)
+#     self.ingredients.update(ingredient)
+# end
+
+
 end
