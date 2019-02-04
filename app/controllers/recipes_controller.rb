b985434fb0ecbf57405fb76b1a492831318d2728
George  Pianka <gpianka1@gmail.com>
2019-02-04 12:25:59 +0000

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,7 @@
 class RecipesController < ApplicationController
+  before_action :set_recipe!, only: %i(show update)
+  
   def show
-    @recipe = Recipe.find(params[:id])
   end
 
   def index
@@ -9,8 +10,34 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    2.times { @recipe.ingredients.build }
   end
 
   def create
+    @recipe = Recipe.create(recipe_params)
+    redirect_to @recipe
   end
+
+  def update
+    recipe.update(recipe_params)
+    redirect_to @recipe
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(
+      :title,
+      ingredients_attributes: [
+        :id,
+        :name,
+        :quantity
+      ]
+    )
+  end
+  
+  def set_recipe!
+    @recipe = Recipe.find(params[:id])
+  end
+    
 end
