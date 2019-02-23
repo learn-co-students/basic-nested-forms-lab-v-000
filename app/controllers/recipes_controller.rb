25504bd958342526f32f95d1774bd899410f272f
Rachel Wyatt <rw126@evansville.edu>
2019-02-23 22:44:19 +0000

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,20 +9,23 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
-    @recipe.ingredients.build(ingredient_type: '1')
-    @recipe.ingredients.build(ingredient_type: '2')
+    @recipe.ingredients.build
+    @recipe.ingredients.build
   end
 
   def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to recipe_path(@recipe), notice: "Your recipe was created"
+    end
   end
 
   def recipe_params
     params.require(:recipe).permit(
     :title,
-    recipe_attributes: [
-      :ingredient_name,
-      :ingredient_amount,
-      :ingredient_type
+    ingredients_attributes: [
+      :name,
+      :quantity,
     ]
     )
   end
