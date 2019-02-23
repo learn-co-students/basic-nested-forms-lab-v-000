1a297a72f88b1f7e215847718c52e0fa3fe99a7f
Rachel Wyatt <rw126@evansville.edu>
2019-02-23 15:46:02 +0000

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,21 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build(ingredient_type: '1')
+    @recipe.ingredients.build(ingredient_type: '2')
   end
 
   def create
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(
+    :title,
+    recipe_attributes: [
+      :ingredient_name,
+      :ingredient_amount,
+      :ingredient_type
+    ]
+    )
   end
 end
