18fb2249bde29b972a1a65301f3f76e8bdca488f
Leslie Stevens <flymaven@protonmail.com>
2019-03-23 15:46:23 -0500

    build form and edit controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,16 +1,41 @@
 class RecipesController < ApplicationController
-  def show
-    @recipe = Recipe.find(params[:id])
-  end
+  before_action :set_recipe!, only:[:show]
 
   def index
     @recipes = Recipe.all
   end
 
+  def show
+
+  end
+
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build(name: 'name')
+    @recipe.ingredients.build(quantity: 'quantity')
   end
 
   def create
+    @recipe = Recipe.new(recipe_params)
+#binding.pry
+    @recipe.save
+    redirect_to recipe_path(@recipe)
   end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(
+      :title,
+      ingredients_attributes: [
+        :id,
+        :name,
+        :quantity
+        ]
+      )
+  end
+
+  def set_recipe!
+  @recipe = Recipe.find(params[:id])
+end
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,3 +1,7 @@
 class Ingredient < ActiveRecord::Base
   belongs_to :recipe
+
+  #def ingredient_attributes=(ingredient)
+  #  self.ingredient = Ingredient.find_by(name: ingredient.name)
+  #end
 end
