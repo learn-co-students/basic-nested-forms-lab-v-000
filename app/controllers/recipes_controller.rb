11e86ace546371bbe89b63972ef0d9d9b4226fb9
Ashley <ashleywong678@gmail.com>
2019-04-10 22:18:21 -0400

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -12,5 +12,18 @@ class RecipesController < ApplicationController
   end
 
   def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to @recipe
+    else
+      render :new
+    end
   end
+
+  private
+
+    def recipe_params
+      params.require(:recipe).permit(:title)
+    end
+
 end
