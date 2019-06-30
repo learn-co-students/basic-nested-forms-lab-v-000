f3b480157adf21bfab2306880f56d8ae2b8a5c44
aljimenez5 <arvida.jimenez@gmail.com>
2019-06-29 21:26:04 -0400

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,18 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build(ingredient_number: 'one')
+    @recipe.ingredients.build(ingredient_number: 'two')
   end
 
   def create
+    Recipe.create(recipe_params)
+    redirect_to recipes_path
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, ingredients_attributes: [:name, :quantity, :ingredient_number])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -16,8 +15,9 @@ ActiveRecord::Schema.define(version: 20160114135651) do
   create_table "ingredients", force: :cascade do |t|
     t.string   "name"
     t.string   "quantity"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "ingredient_number"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
     t.integer  "recipe_id"
   end
 
