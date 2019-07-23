5b23590051bc552e17c9183d09fff3b15dbc3029
github username <github email address>
2019-07-23 11:59:23 -0400

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,7 @@
 class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
+    
   end
 
   def index
@@ -9,8 +10,32 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build
+    @recipe.ingredients.build 
+    #arguments not needed because we want to create an empty ingredient object
+    #.build gives associations as an empty object but can add more things via params
   end
 
   def create
+    @recipe = Recipe.create(recipe_params)
+    redirect_to recipe_path(@recipe)
+  end
+  # <%= render 'form', recipe: @recipe %>
+  # data, loadout
+
+  #rails won't let you start the development server until you run that
+  #bin/rails db:migrate RAILS_ENV=development
+  #And starting the server is how we can see what the problem might be
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(
+      :title,
+      ingredients_attributes: [
+        :name,
+        :quantity
+      ]
+    )
   end
 end
